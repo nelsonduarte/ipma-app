9237ad62ab5f1aa34522eb7a290375bc3006878c
Nelson Duarte <nelson.miguel.duarte@outlook.com>
2023-11-27 10:03:32 +0000

    commit da app

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-'''
-request
-No contexto do Flask, request é um objeto importado do módulo Flask que contém todos os dados que o cliente enviou para o servidor como parte de uma solicitação HTTP. O objeto request encapsula informações como cabeçalhos HTTP, dados de formulário, parâmetros de consulta, etc. É normalmente usado em rotas Flask para obter dados enviados pelo cliente.
-
-requests
-O requests é uma biblioteca Python para enviar solicitações HTTP. E frequentemente usado para interagir com APIs web ou para ir buscar conteúdo de websites. Esta biblioteca não faz parte do Flask mas pode ser usada em conjunto com o Flask para obter dados de outras APIs.
-'''
 
 from flask import Flask, render_template, request
 import requests
@@ -19,25 +12,34 @@ API_ENDPOINT = "https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/
 def index():
     if request.method == 'POST':
         cidade_id = request.form.get('cidade')
-        response = requests.get(f"{API_ENDPOINT}{cidade_id}.json", verify=False)
-        dados_clima = response.json()
+        nome_cidade = None  # Vamos preencher isto mais tarde
+        if cidade_id:  # Verifica se cidade_id não é vazio
+            response = requests.get(f"{API_ENDPOINT}{cidade_id}.json", verify=False)
+            dados_clima = response.json()
 
-        if response.status_code == 200:
-            clima = {
-                # 'cidade': dados_clima['owner'],
-                'temperatura_max': dados_clima['data'][0]['tMax'],
-                'temperatura_min': dados_clima['data'][0]['tMin'],
-                'probabilidade_prec': dados_clima['data'][0]['precipitaProb'],
-                'data_consulta': dados_clima['data'][0]['forecastDate']
-            }
+            if response.status_code == 200:
+                clima = {
+                    'temperatura_max': dados_clima['data'][0]['tMax'],
+                    'temperatura_min': dados_clima['data'][0]['tMin'],
+                    'probabilidade_prec': dados_clima['data'][0]['precipitaProb'],
+                    'data_consulta': dados_clima['data'][0]['forecastDate']
+                }
+                # Aqui, você deve adicionar um código para mapear o 'cidade_id' para o nome da cidade
+                if cidade_id == "1010500":
+                    nome_cidade = "Lisboa"
+                elif cidade_id == "1030300":
+                    nome_cidade = "Porto"
+                elif cidade_id == "1141600":
+                    nome_cidade == "Santarém"  # Note que deve ser '=', não '=='
+            else:
+                clima = None
         else:
-            clima = None
+            clima = None  # Defina clima como None se cidade_id for vazio
 
-        return render_template('index.html', clima=clima)
+    return render_template('index.html', clima=clima, nome_cidade=nome_cidade)
 
-    return render_template('index.html')
+    # return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
 
-# python -m pip freeze > requirements
